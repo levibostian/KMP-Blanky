6a266675fb68078af4f55b6f7df064bc139049e0
runner <runner@Mac-1672066890559.local>
2022-12-26 15:06:06 +0000

    Make SPM v19bd96d158 available to use [skip ci]

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/levibostian/KMP-Blanky/earth/levi/shared-kmmbridge/b1845f1240/shared-kmmbridge-b1845f1240.zip"
-let remoteKotlinChecksum = "650483c917d21647102f5bbbc065628b94a3482cf221aa2dc6f8349e18fa920f"
+let remoteKotlinUrl = "https://maven.pkg.github.com/levibostian/KMP-Blanky/earth/levi/shared-kmmbridge/19bd96d158/shared-kmmbridge-19bd96d158.zip"
+let remoteKotlinChecksum = "b1cb05f3c88bf7ad4937a1225d25a32e8f52f472fa9553fa0e3274471db15960"
 let packageName = "dropbox"
 
 let package = Package(
